bff21e1dce95c7f78cf5fa7e6fe4953b8cc3e4ab
VinuthaTJ179 <93989675+VinuthaTJ179@users.noreply.github.com>
2023-11-01 19:54:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const text = 'Mercury'; // The text you want to "type"
+    const container = document.getElementById('dynamic-letters-container');
+
+    // Split the text into individual characters
+    const characters = text.split('');
+
+    // Function to create and animate dynamic letters with a slower transition
+    function createDynamicLetters() {
+        characters.forEach((character, index) => {
+            setTimeout(function () {
+                const letter = document.createElement('span');
+                letter.classList.add('dynamic-letter');
+                letter.textContent = character;
+                container.appendChild(letter);
+
+                // Apply typing animation with slower transition
+                setTimeout(function () {
+                    letter.style.opacity = 1;
+                }, index * 500); // Adjust the time delay for slower typing speed
+            }, index * 500); // Adjust the time delay for slower typing speed
+        });
+    }
+
+    createDynamicLetters();
+});
